c7366ced79a2f94331b5a1e5e9313f310d408967
bacebu4 <vasua14735@icloud.com>
2021-02-19 10:20:13 +0300

    feat: added new color

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -3,6 +3,7 @@ export const DARK = "#343434";
 export const PINK = "#FE9CA4";
 export const GRAY = "#B0AFAF";
 export const WHITE = "#000000";
+export const OFF_WHITE = "#F9F9F9";
 
 const colors = {
   purple: PURPLE,
@@ -10,9 +11,10 @@ const colors = {
   pink: PINK,
   gray: GRAY,
   white: WHITE,
+  "off white": OFF_WHITE,
 };
 
-export type Color = "purple" | "dark" | "pink" | "gray" | "white";
+export type Color = "purple" | "dark" | "pink" | "gray" | "white" | "off white";
 
 export function colorSwitcher(color: Color): string {
   return colors[color];
