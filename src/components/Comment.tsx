32d777322b2496035ce7e87e9f7ba9af6d37cdc7
vlad99902 <legkovv@mail.ru>
2021-02-21 00:07:56 +0300

    layout: comment component added

---
+++ src/components/Comment.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { GRAY, WHITE } from "../utils/colors";
+import { FlexBox } from "./FlexBox";
+import { Dots } from "./Dots";
+
+const CommentWrapper = styled.section`
+  background-color: ${WHITE};
+  border-radius: 20px;
+  padding: 32px;
+  font-family: "Abhaya Libre", serif;
+  font-size: 1.125rem;
+`;
+
+const CommentDate = styled.h3`
+  font-size: 0.875rem;
+  color: ${GRAY};
+`;
+
+const TextWrapper = styled.div`
+  text-align: justify;
+  line-height: 1.4;
+  margin-top: 16px;
+`;
+
+const ButtonWrapper = styled.div`
+  cursor: pointer;
+`;
+
+export const Comment: React.FC = () => {
+  return (
+    <CommentWrapper>
+      <FlexBox jc="space-between" ai="center">
+        <CommentDate>2020/08/02</CommentDate>
+        <ButtonWrapper>
+          <Dots />
+        </ButtonWrapper>
+      </FlexBox>
+      <TextWrapper>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, itaque
+        quos vitae quam reiciendis tenetur distinctio officiis corrupti quis
+        ratione cupiditate ex praesentium voluptates sed! Quisquam sint in
+        nostrum porro!
+      </TextWrapper>
+    </CommentWrapper>
+  );
+};
