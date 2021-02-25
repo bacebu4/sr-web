da1dfb2ab7ba615c2f6d2d078eefbb33cb21001a
vlad99902 <legkovv@mail.ru>
2021-02-25 13:57:51 +0300

    refactor: baseinput variant

--- src/components/BaseInput.tsx
+++ src/components/BaseInput.tsx
@@ -1,6 +1,7 @@
 import { InputHTMLAttributes } from "react";
 import styled from "styled-components";
-import { GRAY, DARK, OPACITY10, OPACITY1 } from "../utils/colors";
+import { GRAY, DARK, OPACITY10, OPACITY1, WHITE } from "../utils/colors";
+import { PaperPlane } from "./PaperPlane";
 
 type VariantType = "base" | "send";
 
@@ -11,19 +12,34 @@ type InputAreaStyledType = {
   variant?: VariantType;
 };
 
+const InputWrapper = styled.div`
+  position: relative;
+`;
+
 const InputArea = styled.input<InputAreaStyledType>`
   margin-top: ${(props) => props.mt}px;
   margin-left: ${(props) => props.ml}px;
 
-  width: 100%;
   max-width: ${(props) => props.maxWidth}px;
+  width: 100%;
 
   font-size: 0.875rem;
-  padding: 8px 12px;
+  padding: 10px 16px;
   background-color: ${GRAY}${OPACITY10};
   border-radius: 14px;
   border: 1px solid ${GRAY}${OPACITY1};
   color: ${DARK};
+  outline: none;
+
+  ${(props) => {
+    if (props.variant === "send") {
+      return `
+    background-color: ${WHITE};
+    padding-right: 44px;
+    `;
+    }
+    return "";
+  }}
 
   :focus {
     border: 1px solid ${DARK};
@@ -32,6 +48,14 @@ const InputArea = styled.input<InputAreaStyledType>`
   ::placeholder {
     color: ${GRAY};
   }
+`;
+
+const PaperPlaneWrapper = styled.div`
+  position: absolute;
+
+  bottom: 7px;
+  right: 16px;
+  z-index: 1;
 `;
 
 interface BaseInputType extends InputHTMLAttributes<HTMLInputElement> {
@@ -45,5 +69,14 @@ export const BaseInput: React.FC<BaseInputType> = ({
   variant = "base",
   ...rest
 }) => {
-  return <InputArea variant={variant} {...rest} />;
+  return (
+    <InputWrapper>
+      <InputArea variant={variant} {...rest} />
+      {variant === "send" && (
+        <PaperPlaneWrapper>
+          <PaperPlane />
+        </PaperPlaneWrapper>
+      )}
+    </InputWrapper>
+  );
 };
