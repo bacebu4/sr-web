a742517b6427ad69f2546aabe15fda8cbb13fd7b
vlad99902 <legkovv@mail.ru>
2021-02-25 13:09:03 +0300

    refactor: baseinput maxwidth

--- src/components/BaseInput.tsx
+++ src/components/BaseInput.tsx
@@ -2,9 +2,21 @@ import { InputHTMLAttributes } from "react";
 import styled from "styled-components";
 import { GRAY, DARK, OPACITY10, OPACITY1 } from "../utils/colors";
 
-const InputArea = styled.input<{ mt?: number; ml?: number }>`
+type VariantType = "base" | "send";
+
+type InputAreaStyledType = {
+  mt?: number;
+  ml?: number;
+  maxWidth?: number;
+  variant?: VariantType;
+};
+
+const InputArea = styled.input<InputAreaStyledType>`
   margin-top: ${(props) => props.mt}px;
   margin-left: ${(props) => props.ml}px;
+
+  width: 100%;
+  max-width: ${(props) => props.maxWidth}px;
 
   font-size: 0.875rem;
   padding: 8px 12px;
@@ -25,8 +37,13 @@ const InputArea = styled.input<{ mt?: number; ml?: number }>`
 interface BaseInputType extends InputHTMLAttributes<HTMLInputElement> {
   mt?: number;
   ml?: number;
+  maxWidth?: number;
+  variant?: VariantType;
 }
 
-export const BaseInput: React.FC<BaseInputType> = ({ ...rest }) => {
-  return <InputArea {...rest} />;
+export const BaseInput: React.FC<BaseInputType> = ({
+  variant = "base",
+  ...rest
+}) => {
+  return <InputArea variant={variant} {...rest} />;
 };
