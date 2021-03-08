bc418aadeada49e3d076f284c99ee064fc5cd750
vlad99902 <legkovv@mail.ru>
2021-03-08 22:57:13 +0300

    layout: book component

---
+++ src/components/Book.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import Cover from "../assets/cover.png";
+import { withLayoutStyles } from "./LayoutStyles";
+import { Title } from "./Title";
+
+const BookInfoWrapper = styled.div`
+  margin-top: 16px;
+
+  width: 86px;
+`;
+
+type BookType = {
+  className?: string;
+};
+
+const BookLayout: React.FC<BookType> = ({ className }) => {
+  return (
+    <div className={className}>
+      <img width="86px" src={Cover} alt="Book cover" />
+      <BookInfoWrapper>
+        <Title variant="book" title="Anna Karenina" subtitle="Lev Tolstoy" />
+      </BookInfoWrapper>
+    </div>
+  );
+};
+
+export const Book = withLayoutStyles(BookLayout);
