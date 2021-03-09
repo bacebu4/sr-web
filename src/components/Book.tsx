85bc1b67776d3ec0ba1ecc31b99a63f2cb61dbac
vlad99902 <legkovv@mail.ru>
2021-03-09 20:04:13 +0300

    layout: book component

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -3,23 +3,45 @@ import cover from "../assets/cover.png";
 import { withLayoutStyles } from "./LayoutStyles";
 import { Title } from "./Title";
 
+type VariantType = "large" | "medium";
+
+const BookWrapper = styled.div<{ variant?: VariantType }>`
+  width: ${(p) => (p.variant === "large" ? "109" : "86")}px;
+`;
+
 const BookInfoWrapper = styled.div`
   margin-top: 16px;
-  width: 86px;
+`;
+
+const BookImageWrapper = styled.img`
+  width: inherit;
+  height: auto;
 `;
 
 type BookType = {
   className?: string;
+  variant?: VariantType;
 };
 
-const BookLayout: React.FC<BookType> = ({ className }) => {
+const BookLayout: React.FC<BookType> = ({ className, variant = "medium" }) => {
+  function getTitleVariant(bookVariant: VariantType): "book" | "bookLarge" {
+    if (bookVariant === "large") {
+      return "bookLarge";
+    }
+    return "book";
+  }
+
   return (
-    <div className={className}>
-      <img width="86px" src={cover} alt="Book cover" />
+    <BookWrapper className={className} variant={variant}>
+      <BookImageWrapper src={cover} alt="Book cover" />
       <BookInfoWrapper>
-        <Title variant="book" title="Anna Karenina" subtitle="Lev Tolstoy" />
+        <Title
+          variant={getTitleVariant(variant)}
+          title="Anna Karenina"
+          subtitle="Lev Tolstoy"
+        />
       </BookInfoWrapper>
-    </div>
+    </BookWrapper>
   );
 };
 
